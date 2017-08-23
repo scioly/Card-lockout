cf9398fd72266e7928797628caacc81d1c68a99e
Aaron Adler <aadler@uw.edu>
2017-08-22 18:02:05 -0700

    Added a placeholder client implementation

---
+++ client/README.md
@@ -0,0 +1,9 @@
+# Card-lockout Client
+
+This is a Python script to be run on an Orange Pi. It's job is to activate when there is a card swipe, check if an
+internal button has been pushed, and if it has, activate a relay to allow power to flow from an input to an output
+plug.
+
+## Setup
+
+## Assembly
--- client/main.py
+++ client/main.py
@@ -1,1 +1,19 @@
 import requests
+
+
+def watchInput():
+	while True:
+		swipe = input()
+
+		# check to see that this assumption of ID length is correct
+		cardNumber = swipe[4:11]
+		print( 'The last input was ' + cardNumber )
+
+
+def queryServer():
+	# r = requests.post( {} )
+	pass
+
+
+if __name__ == '__main__':
+	watchInput()
