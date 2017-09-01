5fd393f1d52e3d46f1676b7881fcd3e81829561d
Aaron Adler <aadler@uw.edu>
2017-08-31 17:38:24 -0700

    Added a demo Flask server

---
+++ server/main.py
@@ -0,0 +1,39 @@
+# essential Flask modules
+from flask import Flask
+from flask import request
+from flask import render_template
+# for reading the config file
+import yaml
+# for generating SQL queries in pure Python
+import sql
+# for interfacing with the PostgreSQL database
+import psycopg2
+# for generating JSON to send to the client
+import json
+
+
+# create the flask app
+app = Flask( __name__ )
+
+# a simple hello world route
+@app.route( '/' )
+def hello():
+    return( "<h1><marquee>Hello, world!</marquee></h1>" )
+
+# another route demoing how to use the request parameters
+# try "http://localhost:5555/args?1=2&three=four"
+@app.route( '/args' )
+def returnArgs():
+    return( str( request.args ) )
+
+# run the app if the namespace is main
+if __name__ == '__main__':
+    with open( 'config.yaml' ) as f:
+        config = yaml.load( f )
+
+    # makes your life easier
+    app.debug = True
+    # and, run the app
+    app.run( port=config['serverPort'] )
+    # if you get errors, make sure to make a copy of `config_default.yaml` and
+    # call it `config.yaml`
